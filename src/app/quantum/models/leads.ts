9cda00db863d427121b72113f952e54130063a38
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-06-30 09:10:06 -0500

    added the leads interface

---
+++ src/app/quantum/models/leads.ts
@@ -0,0 +1,9 @@
+export interface Leads {
+    id?: string;
+    company?: string;
+    contactName?: string;
+    status?: string;
+    title?: string;
+    sales?: number;
+    backgroundColor?: string;
+}
